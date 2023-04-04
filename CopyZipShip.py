7cec6990609efc5c87ab6a00ccd23e74ff01342d
Cole Rosner <colerosner@gmail.com>
2023-04-04 10:12:14 -0600

    Added copy functionality before zip and ship.

--- CopyZipShip.py
+++ CopyZipShip.py
@@ -1,16 +1,38 @@
+'''
+Script written by Michael Ennis, ISU and Keith Weber, ISU GIS TReC
+
+Cole Rosner 04 Apr 2023 - Merged CopyLayerFilesToFireFolders.py with fireZip.py
+                          to make CopyZipShip.py
+'''
+# Libraries
 import shutil
 import os
 import subprocess
 
-# Script written by Michael Ennis, ISU and Keith Weber, ISU GIS TReC
 # Get current directory
 directory = os.getcwd()
+
+# Get path to directory of files to be copied 
+copyDir = os.path.join(directory, "CopyToEachFIREFolder")
+
 # Counter for how many folders zipped
 zipCount = 0
-# Iterate over that directory
+# Iterate over current directory
 for filename in os.listdir(directory):
     # If the folder starts with "Fire_" and doesn't end with "zip", zip it
     if(filename.startswith("Fire_") and not (filename.endswith("zip"))):
+        
+        # Check if folder already has the files that need to be copied inside of it
+        for file in os.listdir(copyDir):
+            if os.path.isfile(os.path.join(filename, file)):
+                pass # Do nothing, as the file already exists
+            else:
+                srcPath = os.path.join(copyDir, file)
+                dstPath = os.path.join(filename, file)
+                shutil.copy(srcPath, dstPath)
+                # print("Copied {} to the {} folder.".format(file, filename))
+
+        
         shutil.make_archive(filename,'zip', filename)
         # Increment the zip count
         zipCount+=1
