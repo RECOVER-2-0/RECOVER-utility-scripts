13f8066b29715d8bdbf419415ebcba2d4314e8b3
Cole Rosner <colerosner@gmail.com>
2023-07-10 17:28:08 -0600

    Added portal connection

--- RemoveInvalidFireSubmissions.py
+++ RemoveInvalidFireSubmissions.py
@@ -14,3 +14,16 @@ email address for fires submitted to RECOVER 2.0 is two-fold:
 This script may include other criteria for a shape's exclusion 
 from RECOVER 2.0. Those additional criteria will be listed here.
 '''
+
+# Libraries
+from arcgis.gis import GIS 
+
+# Connect to portal
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+portal = "http://www.arcgis.com"
+print("Logging in to your portal...")
+
+gis = GIS(portal, username, password)
+me = gis.users.me
+print(f"Success. Welcome, {me.firstName}.")
