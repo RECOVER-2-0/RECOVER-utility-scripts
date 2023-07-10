6f5aa8b9b5ff7d833351863a04ce1ba410e76177
Cole Rosner <colerosner@gmail.com>
2023-07-10 17:07:23 -0600

    Creation of RemoveInvalidFireSubmissions.py

---
+++ RemoveInvalidFireSubmissions.py
@@ -0,0 +1,16 @@
+'''
+This script removes records from the Community Fire Submissions
+hosted feature layer on ArcGIS Online. Fire shapes and attributes
+will be permanently deleted if the creator of a shape does not 
+provide a valid email address. The purpose of requiring a valid 
+email address for fires submitted to RECOVER 2.0 is two-fold: 
+    
+    1. Allows a communication avenue between RECOVER 2.0
+    maintainers and the post-wildfire decision-making 
+    community
+    2. Helps to prevent spam and therefore improves 
+    RECOVER 2.0's automated data publishing processes
+
+This script may include other criteria for a shape's exclusion 
+from RECOVER 2.0. Those additional criteria will be listed here.
+'''
