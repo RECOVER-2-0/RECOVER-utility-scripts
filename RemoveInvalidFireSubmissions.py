223fc19cf6ad774b7f92fcc3c8e6a0937645e131
Cole Rosner <colerosner@gmail.com>
2023-07-17 09:40:18 -0600

    Added loop deleting fires submitted with bad email

--- RemoveInvalidFireSubmissions.py
+++ RemoveInvalidFireSubmissions.py
@@ -18,7 +18,7 @@ from RECOVER 2.0. Those additional criteria will be listed here.
 # Libraries
 from arcgis.gis import GIS 
 
-# Connect to portal
+# Connect to portal and log in
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 portal = "http://www.arcgis.com"
@@ -27,3 +27,17 @@ print("Logging in to your portal...")
 gis = GIS(portal, username, password)
 me = gis.users.me
 print(f"Success. Welcome, {me.firstName}.")
+
+# Find Community Fire Submissions feature layer
+fsLayer = gis.content.get('9a36d150d6b7441ba5cd9b0d21fd24b0').layers[0] # get Community Fire Submissions using item ID, and grab the layer that makes up the item
+fsFeatSet = fsLayer.query(where="Email_Val='Invalid'")
+fsFeatures = fsFeatSet.features
+print(f"Found {len(fsFeatures)} fires with invalid email addresses. Proceeding to delete...")
+for each in range(len(fsFeatures)):
+    oidStr = str(fsFeatures[each].get_value('OBJECTID'))
+    subfireName = fsFeatures[each].get_value('Fire_Name')
+    delResult = fsLayer.edit_features(deletes = oidStr)
+    print(f"Deleting {subfireName}.")
+    delResult
+
+print("Finished removing invalid fires from the Community Fire Submissions hosted feature layer.")
